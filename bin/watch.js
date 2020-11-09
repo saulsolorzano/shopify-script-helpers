2c9259c80e4812e554b45eb0ee2a704b6638598a
Saúl Solórzano <hola@saulsolorzano.com>
2020-11-09 13:53:50 -0300

    Updating watcher

--- bin/watch.js
+++ bin/watch.js
@@ -1,5 +1,8 @@
 #! /usr/bin/env node
 const shell 		= require("shelljs");
 const theme			= process.argv.splice(2, process.argv.length - 2)[0];
+console.log('\x1b[34m%s\x1b[0m', `======================================`);
+console.log('\x1b[34m%s\x1b[0m', `Empezando watcher para ${theme}`);
+console.log('\x1b[34m%s\x1b[0m', `======================================`);
 shell.exec(`theme watch --env=${theme} --notify=/var/tmp/theme_ready --allow-live`);
 
